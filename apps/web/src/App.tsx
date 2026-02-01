687510677d203ed87982fa8e1061a76f58ddd497
Fer <fer.falcon.dg@gmail.com>
2026-02-01 12:29:07 -0300

    Checks if wiring is right

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 
 function App() {
+  fetch("https://faqs-accordion-api.onrender.com/health")
+    .then((r) => r.json())
+    .then(console.log)
+    .catch(console.error);
   return (
     <>
       <h1>FAQs</h1>
